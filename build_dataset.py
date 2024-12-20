c6637736e9e8252478a554a5f8b06213354cc1b2
Wuxialang zhou <you@example.com>
2024-12-20 15:05:59 +0800

    x

--- build_dataset.py
+++ build_dataset.py
@@ -19,33 +19,31 @@ class CosyVoice:
         print(audio.shape)
         if audio.shape[1] // sr < 1:
             return None
-        try:
-            audio_16k = torchaudio.functional.resample(audio, sr, 16000).cpu().numpy()
-            audio_24k = torchaudio.functional.resample(audio, sr, 24000)
-            client = self.triton_client
-            # Create inputs for the model
-            audio_input = grpcclient.InferInput("audio_16k", audio_16k.shape, "FP32")
-            audio_input.set_data_from_numpy(audio_16k)
+        audio_16k = torchaudio.functional.resample(audio, sr, 16000).cpu().numpy()
+        audio_24k = torchaudio.functional.resample(audio, sr, 24000)
+        client = self.triton_client
+        # Create inputs for the model
+        audio_input = grpcclient.InferInput("audio_16k", audio_16k.shape, "FP32")
+        audio_input.set_data_from_numpy(audio_16k)
 
-            # Prepare the output request
-            speech_feat_output = grpcclient.InferRequestedOutput("speech_feat")
-            embedding_output = grpcclient.InferRequestedOutput("embedding")
-            prompt_speech_token_output = grpcclient.InferRequestedOutput("prompt_speech_token")
+        # Prepare the output request
+        speech_feat_output = grpcclient.InferRequestedOutput("speech_feat")
+        embedding_output = grpcclient.InferRequestedOutput("embedding")
+        prompt_speech_token_output = grpcclient.InferRequestedOutput("prompt_speech_token")
 
-            # Make the inference request
-            response = client.infer(
-                model_name='ensembled_feature_extractor',
-                inputs=[audio_input],
-                outputs=[speech_feat_output, embedding_output, prompt_speech_token_output]
-            )
+        # Make the inference request
+        response = client.infer(
+            model_name='ensembled_feature_extractor',
+            inputs=[audio_input],
+            outputs=[speech_feat_output, embedding_output, prompt_speech_token_output]
+        )
 
-            # Extract the output data
-            embedding = response.as_numpy("embedding")
-            speech_token = response.as_numpy("prompt_speech_token")
-            torchaudio.save(audio_path + '.24k.wav', audio_24k, 24000, encoding="PCM_S", bits_per_sample=16)
-            return audio_path + '.24k.wav', speech_token.tolist(), embedding.tolist()
-        except:
-            return None
+        # Extract the output data
+        embedding = response.as_numpy("embedding")
+        speech_token = response.as_numpy("prompt_speech_token")
+        torchaudio.save(audio_path + '.24k.wav', audio_24k, 24000, encoding="PCM_S", bits_per_sample=16)
+        return audio_path + '.24k.wav', speech_token.tolist(), embedding.tolist()
+
 
     def prepare_ds(self, file_path):
         data = load_dataset('json', data_files=file_path, split='train')
