33b090e1e7e0e7155ea68d996afde052dc63278e
zwxl <zhou_wuxialang@qq.com>
2025-03-14 15:01:45 +0000

    x

--- vocos/dataset.py
+++ vocos/dataset.py
@@ -97,7 +97,7 @@ class CosyvoiceDataset(Dataset):
 
 class VocosDataset(Dataset):
     def __init__(self, cfg: DataConfig, train: bool):
-        with open(cfg.filelist_path) as f:
+        with open(cfg.filelist_path, encoding='utf8') as f:
             self.filelist = f.read().splitlines()
         self.sampling_rate = cfg.sampling_rate
         self.num_samples = cfg.num_samples
